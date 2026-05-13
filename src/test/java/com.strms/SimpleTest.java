ec47a1a2e6c7c315c0f56fa692a883960122858d
enzoblain <enzoblain@proton.me>
2026-05-13 10:55:08 +0200

    feat: test

---
+++ src/test/java/com.strms/SimpleTest.java
@@ -0,0 +1,12 @@
+package com.strms;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SimpleTest {
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}
